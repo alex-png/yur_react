da11036ae7a8707d8d30a1692c1a0d6f8a3adc2a
alex-png <alexrod102797@gmail.com>
2020-05-02 00:42:31 -0400

    styled

---
+++ src/Containers/Chatroom.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Chatroom(){
+
+    return(
+        <div className="chat-room">
+            <div className="messages">
+
+            </div>
+            <div className="input-section">
+            <textarea className='chat-input'></textarea>
+            </div>
+        </div>
+    )
+}
+
+export default Chatroom
--- src/Containers/Main.js
+++ src/Containers/Main.js
@@ -2,13 +2,15 @@ import React from 'react';
 import SideBar from './SideBar'
 import Top from '../Components/Top'
 import '../main.css'
+import Chatroom from './Chatroom';
 
 function Main() {
     return (
         <div className="main">
             <SideBar />
             <div name="Body">
-                <Top style={{pointerEvents: "none"}}/>
+                <Top style={{pointerEvents: "none", cursor:"none"}} />
+                <Chatroom />
             </div>
         </div>)
 }
