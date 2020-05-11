6260d3849f1e410821f646afb507f56756c9b56a
alex-png <alexrod102797@gmail.com>
2020-05-11 14:41:51 -0400

    custom text area

--- src/Containers/Chatroom.js
+++ src/Containers/Chatroom.js
@@ -1,17 +1,42 @@
 import React from 'react'
+import Messages from './Messages'
 
-function Chatroom(){
+class Chatroom extends React.Component {
+    state = {
+        message: ""
+    }
 
-    return(
-        <div className="chat-room">
-            <div className="messages">
+    handleSubmit = (e) => {
+        e.preventDefault();
+        let message = this.state.message;
+        //send message to backend
+        console.log(message)
+        this.setState({message:""})
+    }
 
+    handleChange = (e) => {
+        this.setState({
+            message: e.target.value
+        })
+    }
+    handleKeyDown = (e) => {
+        if (e.key === "Enter" && !e.shiftKey) {
+            this.handleSubmit(e)
+        }
+    }
+
+    render() {
+        return (
+            <div className="chat-room">
+                <Messages />
+                <form className="input-section">
+                    <textarea className='chat-input' value={this.state.message} onChange={this.handleChange} onKeyDown={this.handleKeyDown} />
+                </form>
             </div>
-            <div className="input-section">
-            <textarea className='chat-input'></textarea>
-            </div>
-        </div>
-    )
+
+        
+)
+    }
 }
 
 export default Chatroom
---
+++ src/Containers/Messages.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default class Messages extends React.Component {
+
+
+
+
+    render() {
+        return (
+            <div className="messages">
+            </div>
+        )
+    }
+}
